d5ba7db05a8c9fad40c2854653e325c30b6ffe31
末吉智一 <sueyoshitomokazu@sueyoshitomokazunoMacBook-Air.local>
2023-12-25 10:48:59 +0900

    管理者権限の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-50.times do |n|
-  Post.create!(
-    # n + 1で数字が重複しないようにする
-    body: "ultra_test#{n + 1}",
-    user_id: 1,
-    genre: "Baseball",
-    start_time: DateTime.now,
-    location: "新宿公園"
-  )
-end
+# 50.times do |n|
+#   Post.create!(
+#     # n + 1で数字が重複しないようにする
+#     body: "ultra_test#{n + 1}",
+#     user_id: 1,
+#     genre: "Baseball",
+#     start_time: DateTime.now,
+#     location: "新宿公園"
+#   )
+# end
+
+User.find_by(name: "tomokazu").update!(role: "admin")
